5707e4beefd8deb02f19f4499261b51a2f71a75f
MileneDelCarpioRojas <101756550+MileneDelCarpioRojas@users.noreply.github.com>
2022-09-13 04:36:11 -0500

    Create variable.js

---
+++ Fundamentos-de-JavaScript/ExperienciaPractica2/variable.js
@@ -0,0 +1,50 @@
+let miVar = "prueba";
+let miNum = 987;
+let miBool = true;
+const miConstante = 160;
+
+if(true){
+    let variable1 = "Probando JavaScript";
+    const miConstante =160;
+    console.log("Variable1: "+variable1);
+    console.log("miConstante: "+miConstante);
+}
+
+console.log(x== undefined);
+var x=3;
+var myvar = 'my value';
+
+(function(){
+    console.log(miVar);
+    var myvar= 'valor local';
+})();
+
+var x = "función alterna";
+FuncionPrueba();
+function FuncionPrueba() {
+ console.log("funcion interna");
+ console.log(x);
+}
+console.log("funcion externa");
+console.log(x);
+
+var answer = 42;
+answer = 'Gracias por todo el pescado...';
+
+parseInt("F", 16);
+parseInt("17", 8);
+parseInt("15", 10);
+parseInt(15.99, 10);
+parseInt("FXX123", 16);
+parseInt("1111", 2);
+parseInt("15*3", 10);
+parseInt("12", 13);
+parseInt("Hello", 8);
+parseInt("0x7", 10);
+parseInt("546", 2);
+
+var howMany = 10;
+alert("howMany.toString() is " + howMany.toString());
+alert("45 .toString() is " + 45 .toString());
+var x = 7;
+alert(x.toString(2));
