fa12a00615d2ffe3f7a4e220f89706574b8fa1cf
MileneDelCarpioRojas <101756550+MileneDelCarpioRojas@users.noreply.github.com>
2022-10-25 01:47:09 -0500

    Update views.py

--- Flask_Django/ExperienciaPractica4/views.py
+++ Flask_Django/ExperienciaPractica4/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
-# Create your views here.
+# Create your views here. 
 
-from django.http import HttpResponse
-from django.template import loader
 
-def index(request):
-    template = loader.get_template('pag.html')
-    return HttpResponse(template.render())
+from django.http import HttpResponse 
 
-def ind(request):
-    mensaje="Solicitud completada"
-    return HttpResponse(mensaje)
+from django.template import loader 
+
+
+def index(request): 
+
+    template = loader.get_template('pag.html') 
+
+    return HttpResponse(template.render()) 
+
+
+def ind(request): 
+
+    mensaje="Solicitud completada %r" %request.GET["name"]," se le enviara un correo de respuesta a %r" %request.GET["mail"] 
+
+    return HttpResponse(mensaje) 
