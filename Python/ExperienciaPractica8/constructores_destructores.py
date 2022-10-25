fdcc18fe635a9b6f96831458fe699303b141c16e
MileneDelCarpioRojas <101756550+MileneDelCarpioRojas@users.noreply.github.com>
2022-10-25 00:19:21 -0500

    Create constructores_destructores.py

---
+++ Python/ExperienciaPractica8/constructores_destructores.py
@@ -0,0 +1,15 @@
+#metodo constructor 
+
+def __init__(self, nombre, apellido, codigo): 
+
+        self.nombre= nombre 
+
+        self.apellido=apellido 
+
+        self.codigo=código 
+
+#metodo destructor 
+
+    def del(self): 
+
+        print('Estudiante borrado') 
