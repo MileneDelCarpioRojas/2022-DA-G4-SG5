c80a578286edfe47ce0c206295918b3451977467
Giuliano Zúñiga <103087284+GiuliannoZuniga@users.noreply.github.com>
2022-10-26 20:12:02 -0500

    Add files via upload

---
+++ Python/ExperienciaPractica5/Ejercicio 5 Lectura de Archivo.py
@@ -0,0 +1,7 @@
+# aqui abrimos el archivo que anteriormente escribimos
+# y entonces leemos su contenido y lo mostramos por pantalla
+print('inicio de ejercicio 5\n')
+texto=open('holamundo.txt')
+print(texto.read())
+print('fin de ejercicio 5\n')
+print('\n')
