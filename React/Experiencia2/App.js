ead3e23ec525dd3f1677d2cd4ab58fe54527459c
Giuliano Zúñiga <103087284+GiuliannoZuniga@users.noreply.github.com>
2022-11-05 23:24:33 -0500

    Se usa React Developer Tools

---
+++ React/Experiencia2/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hola a todos, esta es mi primera aplicacion en React</h1>
+      <h2>Aprendiendo a utilizar React</h2>
+      <h3>Esta es otra linea de texto</h3>
+      <h4>Esta aplicacion sera luego revisada utilizando </h4>
+      <h4>React Developer Tools el cual fue añadido a chrome </h4>
+    </div>
+
+  );
+}
+
+export default App; 
