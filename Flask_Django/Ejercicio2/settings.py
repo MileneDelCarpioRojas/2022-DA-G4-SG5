144342e52fd2ae152a49a8cc7172b165b7de75ed
Giuliano Zúñiga <103087284+GiuliannoZuniga@users.noreply.github.com>
2022-11-05 22:35:19 -0500

    Modificaciones a realizar en settings.py

---
+++ Flask_Django/Ejercicio2/settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'mydb',  
+        'USER': 'root',  
+        'PASSWORD': 'clavenueva',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
+    }  
+}
