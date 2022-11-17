0159ce26c65c0b316be02640fb98202494d6688e
RosarioSotoS <101730281+RosarioSotoS@users.noreply.github.com>
2022-11-17 01:03:37 -0500

    NodeJS - Express

---
+++ NodeJS_Express/Ejercicio1/ejercicio1-2/index.js
@@ -0,0 +1,27 @@
+// Solicitando paquete de Mysql
+var mysql = require('mysql');
+
+// Configurando parámetros de conexión (puede variar según instalación)
+var conexion = mysql.createConnection({
+    host:'localhost',
+    port:'3306',
+    database: 'universidad',
+    user: 'root',
+    password: '',
+});
+// Realizando conexión o verificando si sucedió un error
+conexion.connect(function(err){
+    if(err){
+        console.log("Error de conexion"+ err.stack);
+        return;
+    }
+    console.log("Conectado al ID "+ conexion.threadId);
+});
+conexion.query('select * from estudiante', function(error,results){
+    if(error)
+        throw error;
+    results.forEach(element => {
+        console.log(element);
+    });
+});
+conexion.end();
